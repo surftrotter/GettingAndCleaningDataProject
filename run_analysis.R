7c9f4fbce125f073c9d1ceea9b11277954e475e6
rico <riccardo.lowi@gmail.com>
2015-09-20 12:09:49 +0100

    run_analysis final

--- run_analysis.R
+++ run_analysis.R
@@ -6,7 +6,7 @@
 ## 4. Appropriately labels the data set with descriptive variable names. 
 ## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-## Many thanks for your time and feedback.
+## Many thanks for your time and feedback!
 
 ## Here starts the script:
 
